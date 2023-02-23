98e7d280b7864be6304cdfb7a9eb660f0ea2c6f5
Ardduli <ylimartimo.arttu@gmail.com>
2023-02-23 14:43:21 +0200

    Added colorspicker and gridsize-slider
    slider doesn't work atm

--- script.js
+++ script.js
@@ -1,24 +1,87 @@
 const container = document.createElement('div');
-const num = 16;
-const grid = num * num;
 const style = document.createElement('style');
-const perRow =  100/num.toFixed(1)
+let color = '#000000';
+let  mouseDown = false;
+
+// adds controls container
+
+const controls = document.createElement('div');
+controls.classList.add('controls');
+document.body.appendChild(controls);
+
+// create grid slider
+
+const slider = document.createElement('input');
+slider.setAttribute('type','range');
+slider.setAttribute('min','1');
+slider.setAttribute('max','100');
+slider.setAttribute('value','16');
+slider.setAttribute('class','slider');
+controls.appendChild(slider);
+
+//create output for slider value
+
+const output = document.createElement('output');
+let sliderValue = document.querySelector('.slider').value;
+output.textContent = sliderValue;
+controls.appendChild(output);
+
+ oninput = function() {
+    output.textContent = slider.value
+}
+
+slider.addEventListener('mouseup', () => {
+    return sliderValue = document.querySelector('.slider').value;
+})
+
+// create colorpicker
+const colorPicker = document.createElement('input');
+    colorPicker.setAttribute('type','color');
+    colorPicker.setAttribute('id','colorPicker');
+controls.appendChild(colorPicker);
+
+colorPicker.addEventListener('input', () => {
+    return color = colorPicker.value;
+})
+
+
+
+// values for the grid
+num = 16
+const grid = num * num;
+const perRow =  100/num;   
+
+
+//creates the container for the grid
 
 container.classList.add('container');
     
 document.body.appendChild(container);
 
+
+// creates the quares to the grid
 for(i = 0; i < grid; i++){
     const square = document. createElement('div');
     square.classList.add('square');
-    square.classList.add('blank');
     square.setAttribute('id',`${i}`)
     container.appendChild(square);
-    console.log(i)
 }
 
+
+
+// makes the squares to go on equal rows
 style.innerHTML = `
     .square {
         flex: 1 0 ${perRow}%
     }`;
 document.body.appendChild(style);
+
+ // colors the clicked square
+let squares = document.querySelectorAll('.square');
+
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            let id = (square.id)
+            document.getElementById(id).style.backgroundColor = `${color}`;
+        })
+    })
